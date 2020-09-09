45cea0bef421acf74b49c12cc34dee7b55696374
jerobitaille <jerome.robitaille@polymtl.ca>
2020-09-09 19:46:57 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -4,12 +4,12 @@
 
 def square_root(number: int) -> float:
     # TODO completer la fonction
-    return number **
+    return number ** 0.5
 
 
 def square(number: int) -> int:
     # TODO completer la fonction
-    return squared
+    return number ** 2
 
 
 def main() -> None:
